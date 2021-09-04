1cbd07ac1fa425006b3da7c94a841bab10c49182
Andrea Wilderman <awilderman@uchc.edu>
2021-09-03 21:42:40 -0400

    Create make_interact.sh

---
+++ make_interact.sh
@@ -0,0 +1,13 @@
+# Convert loops.bedgraph to .interact format for UCSC Genome Browser
+export OUTDIR=/home/FCAM/awilderman/ANALYSIS/HiC/CNCC/HiCExplorer
+export LABEL=H9
+export RESOLUTION=10000
+export REGION=chr7:25000000-28000000
+
+cd "$OUTDIR"
+echo "cat "$LABEL"_"$RESOLUTION"_loops.bedgraph | \
+awk '{print \$1\"\t\"\$2\"\t\"\$6\"\t"$LABEL"_"$RESOLUTION"_\"\$1\":\"\$2\"-\"\$6\"\t0\t\"\$7\"\t"$LABEL"_"$RESOLUTION"\t0,0,0\t\"\$1\"\t\"\$2\"\t\"\$3\"\t\"\$1\":\"\$2\"-\"\$3\"\t.\t\"\$4\"\t\"\$5\"\t\"\$6\"\t\"\$4\":\"\$5\"-\"\$6\"\t.\"}' > "$LABEL"_"$RESOLUTION".interact
+sed -i -e '1i \
+track type=interact name="interact "$LABEL"_"$RESOLUTION"" description=""$LABEL" loops at "$RESOLUTION"" interactDirectional=true maxHeightPixels=200:100:50 visibility=full\\nbrowser position "$REGION"' "$LABEL"_"$RESOLUTION".interact"> write_interact.sh
+
+# Double-check the track line of the .interact file after sed, some versions don't introduce the newline as expected
